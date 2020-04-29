07149db1ec6e846316897a39e3c3b673a35d9f04
Ygor Martins <ygor_vinicius@hotmail.com>
2020-04-29 20:21:58 -0300

    Projeto Criado

---
+++ src/test/java/com/springproject/restspring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
